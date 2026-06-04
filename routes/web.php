c9b406785d753265dc3d2d2db8f6f2405c59cacc
Dhara-Ghadiya <dhara.ghadiya@nexuslinkservices.in>
2026-06-04 14:19:10 +0530

    remove unused code

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,8 @@
 <?php
 
-use App\Http\Controllers\RedditPostController;
 use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [SearchController::class, 'index'])->name('home');
 Route::post('/searches', [SearchController::class, 'store'])->name('searches.store');
 Route::get('/searches/{search}', [SearchController::class, 'show'])->name('searches.show');
-
-Route::get('/reddit-posts', [RedditPostController::class, 'index'])->name('reddit-posts.index');
-Route::post('/reddit-posts', [RedditPostController::class, 'store'])->name('reddit-posts.store');
-Route::delete('/reddit-posts/{redditPost}', [RedditPostController::class, 'destroy'])->name('reddit-posts.destroy');
